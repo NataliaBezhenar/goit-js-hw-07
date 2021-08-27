cde6a24ea1970724988b34b9ad1eee1aef1d2170
Natalia Bezhenar <natalia.bezhenar@gmail.com>
2021-08-27 17:10:03 +0300

    refactoring

--- js/task-06.js
+++ js/task-06.js
@@ -3,10 +3,18 @@ const numberOfSymbols = parseInt(input.getAttribute("data-length"));
 
 input.addEventListener("focusout", function () {
   if (input.value.length === numberOfSymbols) {
-    input.classList.remove("invalid");
-    input.classList.add("valid");
+    clearStyles("invalid");
+    addStyles("valid");
   } else {
-    input.classList.remove("valid");
-    input.classList.add("invalid");
+    clearStyles("valid");
+    addStyles("invalid");
   }
 });
+
+function clearStyles(style) {
+  input.classList.remove(style);
+}
+
+function addStyles(style) {
+  input.classList.add(style);
+}
